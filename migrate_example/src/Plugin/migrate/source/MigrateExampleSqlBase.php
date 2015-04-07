abb5f3d56418daae2d682f018fdb02ee67358af5
Mike Ryan <mike.ryan@acquia.com>
2015-04-07 09:14:05 -0500

    Issue #2466749 by mikeryan: Fix migrate_example_setup for recent core changes

--- migrate_example/src/Plugin/migrate/source/MigrateExampleSqlBase.php
+++ migrate_example/src/Plugin/migrate/source/MigrateExampleSqlBase.php
@@ -10,13 +10,10 @@ namespace Drupal\migrate_example\Plugin\migrate\source;
 use Drupal\Core\Plugin\ContainerFactoryPluginInterface;
 use Drupal\migrate\Entity\MigrationInterface;
 use Drupal\migrate\Plugin\migrate\source\SqlBase;
-use Drupal\migrate\Plugin\RequirementsInterface;
 use Symfony\Component\DependencyInjection\ContainerInterface;
 
 /**
  * A base source class for beer tables.
- *
- *
  */
 abstract class MigrateExampleSqlBase extends SqlBase implements ContainerFactoryPluginInterface {
 
@@ -24,7 +21,6 @@ abstract class MigrateExampleSqlBase extends SqlBase implements ContainerFactory
    * {@inheritdoc}
    */
   public static function create(ContainerInterface $container, array $configuration, $plugin_id, $plugin_definition, MigrationInterface $migration = NULL) {
-    $configuration['key'] = 'default';
     return new static(
       $configuration,
       $plugin_id,
